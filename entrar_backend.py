91b6afa96b2095c4c593e55ef7ba492abcb99cd0
fw-real <nostorian@proton.me>
2024-04-10 18:12:29 +0530

    real
    realest

---
+++ entrar_backend.py
@@ -0,0 +1,300 @@
+# made withhands by fw-real, discord: @nostorian
+import os
+import re
+import json
+import time
+import requests
+import threading
+from pystyle import *
+from bs4 import BeautifulSoup
+from datetime import datetime
+from colorama import Fore, init
+import chromedriver_autoinstaller
+from seleniumwire import webdriver
+
+
+
+class Scraper:
+    """
+    A class used to scrape assignments and announcements from entrar.in
+
+    ## Parameters
+    ----------
+    username : str
+        The username of the student
+    password : str
+        The password of the student
+    save_data : bool
+        Whether to save the scraped data to a json file or not
+        By default it is set to False
+
+    ## Methods
+    ----------
+    scrape_assignments(subject)
+        Scrapes the assignments of the given subject and returns a list of dictionaries
+    scrape_announcements()
+        Scrapes the announcements and returns a list of dictionaries
+    join_online_class()
+        Joins the first online class available for today
+
+
+    """
+    def __init__(self, username, password, save_data=False):
+        self.username = username
+        self.password = password
+        self.save_data = save_data
+        self.subject_dict = {"physics": "91", "english": "92", "maths": "98", "computers": "124", "chemistry": "138", "economics": "139"}
+        self.session = None
+    
+    def _download_chrome_driver(self):
+        chromedriver_autoinstaller.install()
+
+    def _login(self):
+        login_url = 'https://entrar.in/login/auth/'
+        login_payload = {
+            'username': self.username,
+            'password': self.password
+        }
+        self.session = requests.Session()
+        real = self.session.post(login_url, data=login_payload)
+        auth_soup = BeautifulSoup(real.text, 'html.parser')
+        if auth_soup.find('li', class_='user-profile header-notification') == None:
+            raise Exception("Invalid Credentials were provided.")
+        
+
+    def _get_headers(self):
+        phpsessid = self.session.cookies.get('PHPSESSID')
+        serverid = self.session.cookies.get('SERVERID')
+        cflb = self.session.cookies.get('__cflb')
+
+        headers = {
+            "Accept-Language": "en-US,en;q=0.6",
+            "Cookie": f"PHPSESSID={phpsessid}; SERVERID={serverid}; __cflb={cflb}",
+            "Origin": "https://entrar.in",
+            "Referer": "https://entrar.in/pp_assignment/classassignment",
+            "Sec-Ch-Ua": "\"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Brave\";v=\"116\"",
+            "Sec-Ch-Ua-Mobile": "?0",
+            "Sec-Ch-Ua-Platform": "\"Windows\"",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin",
+            "Sec-Gpc": "1",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+        }
+        return headers
+
+    def get_username(self):
+        self._login()
+        headers = self._get_headers()
+        r = self.session.get("https://entrar.in/parent_portal/student_full_profile", headers=headers)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        user_profile = soup.find('li', class_='user-profile header-notification')
+        user_name = user_profile.find('span').text.strip()
+        return user_name
+
+    def scrape_assignments(self, subject):
+        if subject not in self.subject_dict:
+            raise Exception("Subject not found! Either Invalid Credentials or Invalid Subject were provided.")
+        self._login()
+        headers = self._get_headers()
+        data = {"search_assignment": True}
+        r = self.session.post("https://entrar.in/pp_assignment/classassignment", headers=headers, data=data)
+        
+        soup = BeautifulSoup(r.text, 'html.parser')
+        assignment_data = soup.find('div', id=self.subject_dict[subject], class_='panel-collapse collapse in', role="tabpanel")
+        assignment_table = assignment_data.find('table', class_='table table-striped table-bordered table-styling table-xs')
+
+        assignment_list = []
+
+        def scrape_assignment_data(row):
+            columns = row.find_all('td')
+            s_no = columns[0].text.strip()
+            subject_data = columns[1].text.strip()  # convert to list
+            subject, teacher = subject_data.split('/')
+            assignment_tag = columns[2].text.strip()
+            assignment_description = columns[3].text.strip()
+            start_date = columns[4].text.strip() # convert to list
+            start_list = start_date.split('/')
+            attachment_link = columns[6].find('a')['href'] if columns[6].find('a') else None
+
+            text = start_list[0]
+            words = re.findall(r'\b\w+\b', text)
+            start_date = None
+            end_date = None
+
+            # Iterate through the words to find date patterns
+            for i in range(len(words) - 2):
+                if words[i] == "Start" and words[i + 1] == "Date":
+                    start_date = words[i + 2] + "-" + words[i + 3] + "-" + words[i + 4]
+                elif words[i] == "End" and words[i + 1] == "Date":
+                    end_date = words[i + 2] + "-" + words[i + 3] + "-" + words[i + 4]
+
+            # Create a dictionary for the current assignment
+            assignment_dict = {
+                "s_no": s_no,
+                "subject": subject,
+                "teacher": teacher,
+                "assign_tag": assignment_tag,
+                "assign_desc": assignment_description,
+                "start_date": start_date,
+                "end_date": end_date,
+                "attach_link": attachment_link
+            }
+
+            # Append the assignment dictionary to the list
+            assignment_list.append(assignment_dict)
+
+        threads = []
+        for row in assignment_table.find_all('tr')[1:]:
+            thread = threading.Thread(target=scrape_assignment_data, args=(row,))
+            thread.start()
+            threads.append(thread)
+
+        for thread in threads:
+            thread.join()
+
+        if self.save_data == True:
+            with open("assignments.json", "w") as f:
+                json.dump(assignment_list, f, indent=4)
+
+            print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}+{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.GREEN}Assignments saved to {Fore.YELLOW}assignments.json{Fore.RESET}")
+        
+        # what if user wants every assignment link to be downloaded?
+        # problem for another day
+
+        return assignment_list
+
+    def scrape_announcements(self):
+        self._login()
+        headers = self._get_headers()
+        data = {"announcementlist": True, "session": "undefined"}
+        r = self.session.post("https://entrar.in/pp_announcement/announcement", headers=headers, data=data)
+
+        soup = BeautifulSoup(r.text, 'html.parser')
+        announcement_table = soup.find('table', class_='table table-striped table-bordered nowrap', id='simpletable')
+        if announcement_table == None:
+            raise Exception("Invalid Credentials were provided.")
+
+        announcement_list = []
+
+        def scrape_announcement_data(row):
+            columns = row.find_all('td')
+            s_no = columns[0].text.strip()
+            start_date = columns[1].text.strip()
+            announcement_tag = columns[2].text.strip()
+            announcement = columns[3].text.strip()
+            announcement = " ".join(announcement.split())
+            attachment_link = columns[4].find('a')['href'] if columns[4].find('a') else None
+
+            announcement_dict = {
+                "s_no": s_no,
+                "start_date": start_date,
+                "announcement_tag": announcement_tag,
+                "announcement": announcement,
+                "attach_link": attachment_link
+            }
+
+            announcement_list.append(announcement_dict)
+
+        threads = []
+
+        for row in announcement_table.find_all('tr')[1:]:
+            thread = threading.Thread(target=scrape_announcement_data, args=(row,))
+            thread.start()
+            threads.append(thread)
+
+        for thread in threads:
+            thread.join()
+
+        if self.save_data == True:
+            with open("announcements.json", "w") as f:
+                json.dump(announcement_list, f, indent=4)
+
+            print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}+{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.GREEN}Announcements saved to {Fore.YELLOW}announcements.json{Fore.RESET}")
+        return announcement_list
+    
+    def join_online_class(self):
+        self._login()
+        headers = self._get_headers()
+        today = datetime.today()
+        formatted_date = today.strftime("%Y-%m-%d")
+        data = {"filter": True, "from": formatted_date}
+        r = self.session.post("https://entrar.in/classroom_creation_crm_new/s_display", headers=headers, data=data)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        class_table = soup.find('table', class_='table table-striped table-bordered nowrap table-styling text-wrap')
+        if class_table == None:
+            raise Exception("Either Invalid Credentials were provided or no classes are available for today.")
+        
+        class_list = []
+        def scrape_class_data(row):
+            columns = row.find_all('td')
+            s_no = columns[0].text.strip()
+            class_title = columns[1].text.strip()
+            start_date = columns[2].text.strip()
+            duration = columns[3].text.strip()
+            try:
+                join_link = columns[4].find('a')['href']
+            except:
+                join_link = "No Link Available"
+            class_dict = {
+                "s_no": s_no,
+                "class_title": class_title,
+                "start_date": start_date,
+                "duration": duration,
+                "join_link": join_link
+            }
+            class_list.append(class_dict)
+
+        threads = []
+        for row in class_table.find_all('tr')[1:]:
+            thread = threading.Thread(target=scrape_class_data, args=(row,))
+            thread.start()
+            threads.append(thread)
+
+        for thread in threads:
+            thread.join()
+
+        if self.save_data == True:
+            with open("online_classes.json", "w") as f:
+                json.dump(class_list, f, indent=4)
+        
+            print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}+{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.GREEN}Online classes saved to {Fore.YELLOW}online_classes.json{Fore.RESET}")
+        if len(class_list) == 0:
+            raise Exception("No classes available for today.")
+        elif class_list[0]["join_link"] == "No Link Available":
+            raise Exception("No link available for the class.")
+        else:
+            durations = int(class_list[0]['duration'].split(' ')[0])
+            if not os.path.exists("chromedriver.exe"):
+                print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTRED_EX}-{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.RED}ChromeDriver not found in PATH. Downloading...{Fore.RESET}")
+                self._download_chrome_driver()  
+                print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}+{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.GREEN}ChromeDriver downloaded successfully.{Fore.RESET}")
+            driver = webdriver.Chrome()
+            def interceptor(request):
+                del request.headers['Accept-Language']
+                del request.headers['Cookie']
+                del request.headers['Origin']
+                del request.headers['Referer']
+                del request.headers['Sec-Ch-Ua']
+                del request.headers['Sec-Ch-Ua-Mobile']
+                del request.headers['Sec-Ch-Ua-Platform']
+                del request.headers['Sec-Fetch-Dest']
+                del request.headers['Sec-Fetch-Mode']
+                del request.headers['Sec-Fetch-Site']
+                del request.headers['Sec-Gpc']
+                del request.headers['User-Agent']
+                request.headers = headers
+            
+            driver.request_interceptor = interceptor
+            print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}#{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.GREEN}Attempting to join {class_list[0]['class_title']} for {durations} minutes...{Fore.RESET}")
+            # run the code for duration of the class from online_classes.json
+            driver.get(class_list[0]['join_link'])
+            print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}+{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.GREEN}Joined {class_list[0]['class_title']}{Fore.RESET}")
+            # run the code for duration of the class
+            time.sleep(durations * 60)
+            print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}+{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.GREEN}Left {class_list[0]['class_title']}{Fore.RESET}")
+            driver.quit()
+
+
+if __name__ == "__main__":
+    entrar = Scraper("BN/16609", "$CHIRANJEEVI0103$", save_data=True)
---
+++ main.py
@@ -0,0 +1,139 @@
+# made with hands by fw-real, discord: @nostorian
+import os
+import time
+from pystyle import *
+from colorama import Fore, init
+from entrar_backend import Scraper
+
+
+init(convert=True)
+
+def clear():
+    system = os.name
+    if system == 'nt':
+        os.system('cls')
+    elif system == 'posix':
+        os.system('clear')
+    else:
+        print('\n'*120)
+    return
+
+
+
+def main():
+    clear()
+    banner = """
+┏┓   ┏┓           
+┣ ┏┓╋┗┓┏┏┓┏┓┏┓┏┓┏┓
+┗┛┛┗┗┗┛┗┛ ┗┻┣┛┗ ┛ 
+            ┛     
+"""
+    Write.Print(Center.XCenter(banner), color=Colors.red, interval=0.00)
+    print("\n")
+    Write.Print(Center.XCenter("Kindly be mindful of your usage of this tool."), color=Colors.cyan, interval=0.00)
+    print("\n\n")
+    username = Write.Input("(->) Enter your username: ", color=Colors.green, interval=0.00).upper()
+    password = Write.Input("(->) Enter your password: ", color=Colors.green, interval=0.00)
+    scraping_choice = Write.Input("(?) Do you want to scrape data to json(y/n)?: ", color=Colors.light_blue, interval=0.00)
+    # CHECK IF ANY OF THEM IS EMPTY
+    if username == "" or password == "" or scraping_choice == "":
+        print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTRED_EX}!{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.RED}Please fill in all fields.{Fore.RESET}")
+        input()
+        return
+    if scraping_choice == "y" or scraping_choice == "Y":
+        scrape = True
+    elif scraping_choice == "n" or scraping_choice == "N":
+        scrape = False
+    else:
+        print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}-{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.RED}Invalid choice, defaulting to no scraping.{Fore.RESET}")
+        scrape = False
+    e = Scraper(username, password, scrape)
+    try:
+        u = e.get_username()
+        print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}+{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.GREEN}Logged in as {u}{Fore.RESET}")
+    except Exception as e:
+        print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTRED_EX}!{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.RED}An error occurred: {e}{Fore.RESET}")
+        input()
+    
+    # print please wait message
+    print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}#{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.GREEN}Setting up the tool, please wait...{Fore.RESET}")
+    time.sleep(3)
+    clear()
+    
+    # print the banner again
+    Write.Print(Center.XCenter(banner), color=Colors.red, interval=0.00)
+    print("\n")
+    Write.Print(Center.XCenter("Kindly be mindful of your usage of this tool."), color=Colors.cyan, interval=0.00)
+    print("\n")
+    # print logged in as username
+    print(f"\n{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}+{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.GREEN}Logged in as {u}{Fore.RESET}")
+    print("\n\n")
+    # print the options
+    print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}1{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.WHITE}Get Announcements{Fore.RESET}")
+    print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}2{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.WHITE}Get Assignments{Fore.RESET}")
+    print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}3{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.WHITE}Join Online Class{Fore.RESET}")
+    print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}4{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.WHITE}Exit{Fore.RESET}")
+
+    choice = Write.Input("(->) Enter your choice: ", color=Colors.green, interval=0.00)
+    choices = ["1", "2", "3"]
+    if choice not in choices:
+        print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTRED_EX}!{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.RED}Invalid choice, exiting...{Fore.RESET}")
+        input()
+        return
+    if choice == "1":
+        print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}#{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.GREEN}Fetching announcements...{Fore.RESET}")
+        try:
+            announcements = e.scrape_announcements()
+            print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}+{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.GREEN}Announcements fetched successfully.{Fore.RESET}")
+            print("\n")
+            for announcement in announcements:
+                print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}#{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.YELLOW}{announcement['announcement_tag']}: {announcement['announcement']}{Fore.RESET}")
+            input()
+        except Exception as e:
+            print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTRED_EX}!{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.RED}An error occurred: {e}{Fore.RESET}")
+            input()
+    elif choice == "2":
+        # the only subjects available are: {"physics": "91", "english": "92", "maths": "98", "computers": "124", "chemistry": "138", "economics": "139"}
+        # print menu
+        clear()
+        Write.Print(Center.XCenter(banner), color=Colors.red, interval=0.00)
+        print("\n")
+        Write.Print(Center.XCenter("Kindly be mindful of your usage of this tool."), color=Colors.cyan, interval=0.00)
+        print("\n")
+        # print logged in as username
+        print(f"\n{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}+{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.GREEN}Logged in as {u}{Fore.RESET}")
+        print("\n\n")
+        print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}1{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.WHITE}Physics{Fore.RESET}")
+        print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}2{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.WHITE}English{Fore.RESET}")
+        print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}3{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.WHITE}Maths{Fore.RESET}")
+        print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}4{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.WHITE}Computers{Fore.RESET}")
+        print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}5{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.WHITE}Chemistry{Fore.RESET}")
+        print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}6{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.WHITE}Economics{Fore.RESET}")
+        subject = Write.Input("(->) Enter the choice of subject: ", color=Colors.green, interval=0.00)
+        try:
+            a = {"1": "physics", "2": "english", "3": "maths", "4": "computers", "5": "chemistry", "6": "economics"}
+            subject = a[subject]
+            print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}#{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.GREEN}Fetching assignments...{Fore.RESET}")
+            assignments = e.scrape_assignments(subject)
+            print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}+{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.GREEN}Assignments fetched successfully.{Fore.RESET}")
+            print("\n")
+            for assignment in assignments:
+                print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}#{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.GREEN}{assignment['assign_tag']}: {assignment['assign_desc']}:-\n{Fore.WHITE}Assignment Link: {assignment['attach_link']}\nIssued Date: {assignment['start_date']}\nDue Date: {assignment['end_date']}{Fore.RESET}")
+                print("\n\n")
+            input()
+        except Exception as e:
+            print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTRED_EX}!{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.RED}An error occurred: {e}{Fore.RESET}")
+            input()
+    
+    elif choice == "3":
+        print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}#{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.GREEN}Attempting to join online class...{Fore.RESET}")
+        try:
+            e.join_online_class()
+        except Exception as e:
+            print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTRED_EX}!{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.RED}An error occurred: {e}{Fore.RESET}")
+            input()
+    else:
+        print(f"{Fore.LIGHTBLACK_EX}({Fore.RESET}{Fore.LIGHTGREEN_EX}#{Fore.RESET}{Fore.LIGHTBLACK_EX}){Fore.RESET} {Fore.GREEN}Exiting...{Fore.RESET}")
+        input()
+        return
+main()
